fb32362b1fbc28de9d0773e4128bb0e2552cc3ba
Jinzhe Zeng <njzjz@qq.com>
2018-09-28 09:23:52 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 setup(name='CQJobMonitor',
-      version='1.0.1',
+      version='1.0.2',
       description='A PBS job monitor powered by CoolQ.',
       keywords="coolq",
       url='https://github.com/njzjz/CQJobMonitor',
       author='Jinzhe Zeng',
-      author_email='njzjz@qq.com',
+      author_email='jzzeng@stu.ecnu.edu.cn',
       packages=['CQJobMonitor'],
       install_requires=['cqhttp'])
